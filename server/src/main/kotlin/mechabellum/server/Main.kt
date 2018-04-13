bfc3e20f561ef9a139de32b5b74072e9552af35c
Steven Soloff <steven.soloff@gmail.com>
2018-04-12 22:46:13 -0400

    Add shutdown URL

--- server/src/main/kotlin/mechabellum/server/Main.kt
+++ server/src/main/kotlin/mechabellum/server/Main.kt
@@ -18,15 +18,18 @@
 package mechabellum.server
 
 import io.ktor.application.call
+import io.ktor.application.install
 import io.ktor.http.ContentType
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
+import io.ktor.server.engine.ShutDownUrl
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
 fun main(args: Array<String>) {
     val server = embeddedServer(Netty, port = 8080) {
+        install(ShutDownUrl.ApplicationCallFeature) // TODO: need to secure this so it's not open to world
         routing {
             get("/") {
                 call.respondText("Hello World!", ContentType.Text.Plain)
